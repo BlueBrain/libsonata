0bef3cd91e33eee960eefe16a104aae2e2ddfd53
Tristan Carel <tristan.carel@gmail.com>
2019-08-16 16:25:43 +0200

    fixup! Try to fix mac build

--- src/population.hpp
+++ src/population.hpp
@@ -58,8 +58,8 @@ std::vector<T> _readChunk(const HighFive::DataSet& dset, const Selection::Range&
 {
     std::vector<T> result;
     assert (range.first < range.second);
-    size_t chunkSize = range.second - range.first;
-    dset.select({range.first}, {chunkSize}).read(result);
+    auto chunkSize = static_cast<size_t>(range.second - range.first);
+    dset.select({static_cast<size_t>(range.first)}, {chunkSize}).read(result);
     return result;
 }
 
@@ -92,8 +92,8 @@ std::vector<T> _readSelection(const HighFive::DataSet& dset, const Selection& se
     T* dst = result.data();
     for (const Selection::Range& range: selection.ranges()) {
         assert (range.first < range.second);
-        unsigned long long chunkSize = range.second - range.first;
-        dset.select({range.first}, {chunkSize}).read(dst);
+        auto chunkSize = static_cast<size_t>(range.second - range.first);
+        dset.select({static_cast<size_t>(range.first)}, {chunkSize}).read(dst);
         dst += chunkSize;
     }
 
