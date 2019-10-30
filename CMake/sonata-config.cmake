e607f3a6d8e33d74364252b9774c4acdbd304197
Jorge Blanco Alonso <jblanco@cbdhcp-2-003.epfl.ch>
2019-10-30 18:10:29 +0100

    Modified sonata-config.cmake to find include and lib

--- CMake/sonata-config.cmake
+++ CMake/sonata-config.cmake
@@ -1,1 +1,7 @@
+
+get_filename_component(CONFIG_PATH "${CMAKE_CURRENT_LIST_FILE}" PATH)
+find_path(sonatareport_INCLUDE_DIR reportinglib/records.h HINTS ${CONFIG_PATH}/../../../include)
+find_path(sonatareport_LIB_DIR NAMES libsonatareport.so HINTS ${CONFIG_PATH}/../../../lib)
+find_library(sonatareport_LIBRARY sonatareport HINTS ${CONFIG_PATH}/../../../lib)
+
 include("${CMAKE_CURRENT_LIST_DIR}/sonata-targets.cmake")
