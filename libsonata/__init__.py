5f21ca8d3853cc58553dcebbd94dd3341e401eae
Mike Gevaert <michael.gevaert@epfl.ch>
2020-03-12 09:40:26 +0100

    Create python wrapper so that mac wheels will work
    * need to work around:
        https://github.com/matthew-brett/delocate/issues/22

---
+++ libsonata/__init__.py
@@ -0,0 +1,19 @@
+# Needed so that mac wheels properly delocate:
+#  https://github.com/matthew-brett/delocate/issues/22
+
+from libsonata._libsonata import (EdgePopulation,
+                                  EdgeStorage,
+                                  ElementsDataFrame,
+                                  ElementsReportPopulation,
+                                  ElementsReportReader,
+                                  NodePopulation,
+                                  NodeStorage,
+                                  Selection,
+                                  SomasDataFrame,
+                                  SomasReportPopulation,
+                                  SomasReportReader,
+                                  SonataError,
+                                  SpikePopulation,
+                                  SpikeReader,
+                                  version,
+                                  )
