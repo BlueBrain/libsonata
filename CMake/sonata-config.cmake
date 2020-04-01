49f80bf404aca89086776d266c5b1811edd743e2
Blanco Alonso Jorge <jblanco@bb-fvfw811rj1wv.epfl.ch>
2020-04-01 12:57:55 +0200

    fixup! CMake config supports all library suffixes

--- CMake/sonata-config.cmake
+++ CMake/sonata-config.cmake
@@ -1,7 +1,7 @@
 
 get_filename_component(CONFIG_PATH "${CMAKE_CURRENT_LIST_FILE}" PATH)
 find_path(sonatareport_INCLUDE_DIR bbp/sonata/reports.h HINTS ${CONFIG_PATH}/../../../include)
-find_path(sonatareport_LIB_DIR NAMES "libsonatareport.${CMAKE_SHARED_LIBRARY_SUFFIX}" HINTS ${CONFIG_PATH}/../../../lib)
+find_path(sonatareport_LIB_DIR NAMES "libsonatareport${CMAKE_SHARED_LIBRARY_SUFFIX}" HINTS ${CONFIG_PATH}/../../../lib)
 find_library(sonatareport_LIBRARY sonatareport HINTS ${CONFIG_PATH}/../../../lib)
 
 include("${CMAKE_CURRENT_LIST_DIR}/sonata-targets.cmake")
