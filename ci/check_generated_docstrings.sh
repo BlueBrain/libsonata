7ce17625993431a4fc4f886e8221d01c62a8fb52
Andrew Hale <43584291+haleepfl@users.noreply.github.com>
2020-05-14 10:40:14 +0200

    Clean up docstring checking script

--- ci/check_generated_docstrings.sh
+++ ci/check_generated_docstrings.sh
@@ -6,19 +6,15 @@
 set -euo pipefail
 
 VENV=build/venv-docstrings
-CLANG_FORMAT_VERSION=9.0.0
+CLANG_VERSION=6.0.0.2
 
 if [[ ! -d $VENV ]]; then
     python3 -mvenv "$VENV"
-    "$VENV/bin/pip" install clang-format=="$CLANG_FORMAT_VERSION"
+    "$VENV/bin/pip" install clang=="$CLANG_VERSION"
 fi
 
-set +u  # ignore errors in virtualenv's activate
-source "$VENV/bin/activate"
-set -u
-
 # regenerate the docstrings
-python ./python/pybind11/tools/mkdoc.py ./include/bbp/sonata/*.h -o ./python/generated/docstrings.h
+$VENV/bin/python ./python/pybind11/tools/mkdoc.py ./include/bbp/sonata/*.h -o ./python/generated/docstrings.h
 
 # fail if there are diffs in the generated docstrings
 git diff --exit-code -- ./python/generated/docstrings.h
