16469c71069d0942b7677979c9e45bae208dc618
Andrew Hale <43584291+haleepfl@users.noreply.github.com>
2020-05-14 11:09:17 +0200

    Use clang from pybind

--- ci/check_generated_docstrings.sh
+++ ci/check_generated_docstrings.sh
@@ -6,15 +6,10 @@
 set -euo pipefail
 
 VENV=build/venv-docstrings
-CLANG_VERSION=6.0.0.2
-
-if [[ ! -d $VENV ]]; then
-    python3 -mvenv "$VENV"
-    "$VENV/bin/pip" install clang=="$CLANG_VERSION"
-fi
+python3 -mvenv "$VENV"
 
 # regenerate the docstrings
-$VENV/bin/python ./python/pybind11/tools/mkdoc.py ./include/bbp/sonata/*.h -o ./python/generated/docstrings.h
+$VENV/bin/python mkdoc.py ./include/bbp/sonata/*.h -o ./python/generated/docstrings.h
 
 # fail if there are diffs in the generated docstrings
 git diff --exit-code -- ./python/generated/docstrings.h
