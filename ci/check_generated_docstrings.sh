91725554cb6176cac3889ae849ac7df93171b85f
Andrew Hale <43584291+haleepfl@users.noreply.github.com>
2020-05-14 11:20:35 +0200

    Run mkdoc.py from path

--- ci/check_generated_docstrings.sh
+++ ci/check_generated_docstrings.sh
@@ -9,7 +9,7 @@ VENV=build/venv-docstrings
 python3 -mvenv "$VENV"
 
 # regenerate the docstrings
-$VENV/bin/python mkdoc.py ./include/bbp/sonata/*.h -o ./python/generated/docstrings.h
+$VENV/bin/python -m mkdoc ./include/bbp/sonata/*.h -o ./python/generated/docstrings.h
 
 # fail if there are diffs in the generated docstrings
 git diff --exit-code -- ./python/generated/docstrings.h
