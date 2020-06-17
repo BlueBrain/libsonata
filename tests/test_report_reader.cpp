1e25e2a65457e8fc085a29e85a2c7ceaf3934f82
Nicolas Cornu <nicolac76@yahoo.fr>
2020-06-17 16:55:46 +0200

    Add more tests

--- tests/test_report_reader.cpp
+++ tests/test_report_reader.cpp
@@ -4,6 +4,16 @@
 
 using namespace bbp::sonata;
 
+bool testTimes(std::vector<double> vec, double start, double step, int size) {
+    REQUIRE(size == vec.size());
+    for (int i = 0; i < vec.size(); ++i) {
+        if (std::abs(vec[i] - (start + step * i) > std::numeric_limits<double>::epsilon())) {
+            return false;
+        }
+    }
+    return true;
+}
+
 TEST_CASE("SpikeReader", "[base]") {
     const SpikeReader reader("./data/spikes.h5");
 
@@ -23,23 +33,56 @@ TEST_CASE("SpikeReader", "[base]") {
             SpikeReader::Population::Sorting::none);
 }
 
+TEST_CASE("ReportReader limits", "[base]") {
+    const SomaReportReader reader("./data/somas.h5");
+
+    REQUIRE(reader.getPopulationsNames() == std::vector<std::string>{"All", "soma1", "soma2"});
+
+    auto pop = reader.openPopulation("All");
+
+    // ids out of range
+    REQUIRE(pop.get(Selection({{100, 101}})).ids == DataFrame<NodeID>::DataType());
+    REQUIRE(testTimes(pop.get(Selection({{100, 101}})).times, 0.0, 0.1, 10) == true);
+    REQUIRE(pop.get(Selection({{100, 101}})).data ==
+            std::vector<std::vector<float>>({{}, {}, {}, {}, {}, {}, {}, {}, {}, {}}));
+
+    // Inverted id
+    REQUIRE_THROWS(pop.get(Selection({{2, 1}})));
+
+    // Negative ids
+    REQUIRE_THROWS(pop.get(Selection({{-1, 1}})));
+
+    // Times out of range
+    REQUIRE(pop.get(Selection({{1, 2}}), 100., 101.).ids == DataFrame<NodeID>::DataType());
+    REQUIRE(testTimes(pop.get(Selection({{1, 2}}), 100., 101.).times, 0.0, 0.0, 0) == true);
+    REQUIRE(pop.get(Selection({{1, 2}}), 100., 101.).data == std::vector<std::vector<float>>({}));
+
+    // Negatives times
+    REQUIRE(pop.get(Selection({{1, 2}}), -1., -2.).ids == DataFrame<NodeID>::DataType({1}));
+    REQUIRE(testTimes(pop.get(Selection({{1, 2}}), -1., -2.).times, 0.0, 0.1, 10) == true);
+    REQUIRE(pop.get(Selection({{1, 2}}), -1., -2.).data ==
+            std::vector<std::vector<float>>(
+                {{1.0}, {1.1}, {1.2}, {1.3}, {1.4}, {1.5}, {1.6}, {1.7}, {1.8}, {1.9}}));
+}
+
 TEST_CASE("ReportReader", "[base]") {
     const SomaReportReader reader("./data/somas.h5");
 
     REQUIRE(reader.getPopulationsNames() == std::vector<std::string>{"All", "soma1", "soma2"});
 
-    REQUIRE(reader.openPopulation("All").getTimes() == std::make_tuple(0., 1., 0.1));
+    auto pop = reader.openPopulation("All");
 
-    REQUIRE(reader.openPopulation("All").getTimeUnits() == "ms");
+    REQUIRE(pop.getTimes() == std::make_tuple(0., 1., 0.1));
 
-    REQUIRE(reader.openPopulation("All").getDataUnits() == "mV");
+    REQUIRE(pop.getTimeUnits() == "ms");
 
-    REQUIRE(reader.openPopulation("All").getSorted());
+    REQUIRE(pop.getDataUnits() == "mV");
 
-    REQUIRE(reader.openPopulation("All").get(Selection({{3, 5}}), 0.2, 0.5).ids ==
-            DataFrame<NodeID>::DataType{{3, 4}});
+    REQUIRE(pop.getSorted());
 
-    REQUIRE(
-        reader.openPopulation("All").get(Selection({{3, 5}}), 0.2, 0.5).data ==
-        std::vector<std::vector<float>>{{{3.2f, 4.2f}, {3.3f, 4.3f}, {3.4f, 4.4f}, {3.5f, 4.5f}}});
+    auto data = pop.get(Selection({{3, 5}}), 0.2, 0.5);
+    REQUIRE(data.ids == DataFrame<NodeID>::DataType{{3, 4}});
+    REQUIRE(testTimes(data.times, 0.2, 0.1, 4) == true);
+    REQUIRE(data.data == std::vector<std::vector<float>>{
+                             {{3.2f, 4.2f}, {3.3f, 4.3f}, {3.4f, 4.4f}, {3.5f, 4.5f}}});
 }
