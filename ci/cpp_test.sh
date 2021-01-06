147d904bb8c3d1d24ca03b1d2f56ac0b466a8cdd
Mike Gevaert <michael.gevaert@epfl.ch>
2021-01-06 15:12:12 +0100

    remove travis, switch to github actions for tests
    * tests first run the CPP w/ clang & gcc, then the python ones, then the
      macos ones
    * only support running tests on py3

--- ci/cpp_test.sh
+++ ci/cpp_test.sh
@@ -9,9 +9,17 @@ BUILD_DIR=build/build-cpp-test
 rm -rf "$BUILD_DIR"
 mkdir -p "$BUILD_DIR"
 cd "$BUILD_DIR"
-cmake                           \
-    -DCMAKE_BUILD_TYPE=Release  \
-    -DEXTLIB_FROM_SUBMODULES=ON \
-    -DSONATA_CXX_WARNINGS=ON    \
+
+SONATA_VERSION=
+if ! git describe --tags > /dev/null 2>&1; then
+    SONATA_VERSION='-DSONATA_VERSION=0.0.0'
+    echo "Likely shallow git clone, faking SONATA_VERSION -> $SONATA_VERSION"
+fi
+
+cmake $SONATA_VERSION                                                      \
+    -DCMAKE_BUILD_TYPE=Release                                             \
+    -DEXTLIB_FROM_SUBMODULES=ON                                            \
+    -DSONATA_CXX_WARNINGS=ON                                               \
     ../..
+
 make -j all test
