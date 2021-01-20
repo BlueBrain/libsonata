39e9c3c5f324e46fafa79486d943eaae753ad46c
Mike Gevaert <michael.gevaert@epfl.ch>
2021-01-20 15:31:12 +0100

    quote all variables in python_build_sdist.sh in case of spaces

--- ci/python_build_sdist.sh
+++ ci/python_build_sdist.sh
@@ -6,22 +6,22 @@ set -euo pipefail
 
 BASE=$(pwd)/build/
 
-mkdir -p $BASE
+mkdir -p "$BASE"
 
-VENV=$BASE/venv-python-sdist/
+VENV="$BASE/venv-python-sdist/"
 if [[ ! -d "$VENV" ]]; then
     python3 -mvenv "$VENV"
 fi
-BIN=$VENV/bin/
+BIN="$VENV/bin/"
 
 set +u  # ignore missing variables in activation script
-source $BIN/activate
+source "$BIN/activate"
 set -u
 
-$BIN/pip install -U setuptools pip twine
-$BIN/python setup.py sdist
+"$BIN/pip" install -U setuptools pip twine
+"$BIN/python" setup.py sdist
 
 ls -al dist
 
-cd $VENV
-$BIN/pip --verbose install ../../dist/*.tar.gz
+cd "$VENV"
+"$BIN/pip" --verbose install ../../dist/*.tar.gz
