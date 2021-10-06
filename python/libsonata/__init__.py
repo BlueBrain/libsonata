8801349b277acc4efebfcc8de618f1f8f94e95a3
Matthias Wolf <matthias.wolf@epfl.ch>
2021-10-06 08:56:55 +0200

    Have the shim module document functionality (#162)

--- python/libsonata/__init__.py
+++ python/libsonata/__init__.py
@@ -1,21 +1,44 @@
+"""Python reader for SONATA circuit files
+"""
 # Needed so that mac wheels properly delocate:
 #  https://github.com/matthew-brett/delocate/issues/22
 
-from libsonata._libsonata import (CircuitConfig,
-                                  EdgePopulation,
-                                  EdgeStorage,
-                                  ElementDataFrame,
-                                  ElementReportPopulation,
-                                  ElementReportReader,
-                                  NodePopulation,
-                                  NodeSets,
-                                  NodeStorage,
-                                  Selection,
-                                  SomaDataFrame,
-                                  SomaReportPopulation,
-                                  SomaReportReader,
-                                  SonataError,
-                                  SpikePopulation,
-                                  SpikeReader,
-                                  version,
-                                  )
+from libsonata._libsonata import (
+    CircuitConfig,
+    EdgePopulation,
+    EdgeStorage,
+    ElementDataFrame,
+    ElementReportPopulation,
+    ElementReportReader,
+    NodePopulation,
+    NodeSets,
+    NodeStorage,
+    Selection,
+    SomaDataFrame,
+    SomaReportPopulation,
+    SomaReportReader,
+    SonataError,
+    SpikePopulation,
+    SpikeReader,
+    version,
+)
+
+__all__ = [
+    "CircuitConfig",
+    "EdgePopulation",
+    "EdgeStorage",
+    "ElementDataFrame",
+    "ElementReportPopulation",
+    "ElementReportReader",
+    "NodePopulation",
+    "NodeSets",
+    "NodeStorage",
+    "Selection",
+    "SomaDataFrame",
+    "SomaReportPopulation",
+    "SomaReportReader",
+    "SonataError",
+    "SpikePopulation",
+    "SpikeReader",
+    "version",
+]
