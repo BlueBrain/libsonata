7a3983a4ac89535550075feafd5338a1693a1f5f
Luc Grosheintz <luc.grosheintz@gmail.com>
2023-10-20 10:14:58 +0200

    Formatting CI prints diff on failure. (#295)

--- ci/check_clang_format.sh
+++ ci/check_clang_format.sh
@@ -23,5 +23,8 @@ changes=$(git-clang-format 'HEAD~1' $DIRS_TO_FORMAT)
 if [[ $(echo "$changes" | grep -n1 'changed files') ]]; then
     echo "The following files require changes to pass the current clang-format"
     echo "$changes"
+    echo ""
+    echo "This is the diff:"
+    git diff
     exit 1
 fi
