3accbdcd30c97268799bf50bef2dbaeb21d3c77c
Luc Grosheintz <luc.grosheintz@gmail.com>
2023-11-13 16:00:02 +0100

    Refactor `_readSelection`.
    This commit refactors `_readSelection` in such a manner that:
      1. Canonical selections don't require post-read shuffling.
      2. The reading of the dataset is moved to a separate function.
    
    As a consequence, there's no need for an optimization for `std::string`, since
    those only mattern when reading from the `"@library"`, which should only happen
    indirectly. Hence, we free to ensure that those are always canonical.

--- src/read_canonical_selection.hpp
+++ src/read_canonical_selection.hpp
@@ -1,10 +1,30 @@
 #pragma once
 
+#include <vector>
+
 #include "read_bulk.hpp"
+#include <highfive/H5File.hpp>
 
 namespace bbp {
 namespace sonata {
 namespace detail {
+
+template <class Range>
+HighFive::HyperSlab make_hyperslab(const std::vector<Range>& ranges) {
+    HighFive::HyperSlab slab;
+    for (const auto& range : ranges) {
+        size_t i_begin = std::get<0>(range);
+        size_t i_end = std::get<1>(range);
+        slab |= HighFive::RegularHyperSlab({i_begin}, {i_end - i_begin});
+    }
+
+    return slab;
+}
+
+template <class T>
+std::vector<T> readCanonicalSelection(const HighFive::DataSet& dset, const Selection& selection) {
+    return dset.select(make_hyperslab(selection.ranges())).template read<std::vector<T>>();
+}
 
 template <class T, class XRange, class YRange>
 std::vector<T> readCanonicalSelection(const HighFive::DataSet& dset,
