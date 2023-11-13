06a53909e50ee2433e134e562fca8059d07b1311
Luc Grosheintz <luc.grosheintz@gmail.com>
2023-11-13 14:05:40 +0100

    Refactor `edge_index::resolve`.
    The point is to split reading of the dataset into a separate function, and then
    make `resolve` safe for collective IO (assuming the newly introduced function
    is).
    
    The overload for reading a single `nodeID` is removed as it's unused now.

--- src/read_canonical_selection.hpp
+++ src/read_canonical_selection.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
 #include <vector>
-
 #include <highfive/H5File.hpp>
+
+#include "read_bulk.hpp"
 
 namespace bbp {
 namespace sonata {
 namespace detail {
-
 
 template <class Range>
 HighFive::HyperSlab make_hyperslab(const std::vector<Range>& ranges) {
@@ -28,6 +28,33 @@ std::vector<T> readCanonicalSelection(const HighFive::DataSet& dset, const Selec
     }
 
     return dset.select(make_hyperslab(selection.ranges())).template read<std::vector<T>>();
+}
+
+template <class T, class XRange, class YRange>
+std::vector<T> readCanonicalSelection(const HighFive::DataSet& dset,
+                                      const std::vector<XRange>& xranges,
+                                      const std::vector<YRange>& yranges) {
+    if (yranges.size() != 1) {
+        throw SonataError("Only yranges.size() == 1 has been implemented.");
+    }
+
+    size_t j_begin = std::get<0>(yranges[0]);
+    size_t j_end = std::get<1>(yranges[0]);
+
+    constexpr size_t min_gap_size = SONATA_PAGESIZE / (2 * sizeof(uint64_t));
+    constexpr size_t max_aggregated_block_size = 128 * min_gap_size;
+
+    auto readBlock = [&](auto& buffer, const auto& xrange) {
+        size_t i_begin = std::get<0>(xrange);
+        size_t i_end = std::get<1>(xrange);
+        dset.select({i_begin, j_begin}, {i_end - i_begin, j_end - j_begin}).read(buffer);
+    };
+
+    return bulk_read::bulkRead<T>([&readBlock](auto& buffer,
+                                               const auto& range) { readBlock(buffer, range); },
+                                  xranges,
+                                  min_gap_size,
+                                  max_aggregated_block_size);
 }
 
 }  // namespace detail
