01f10458c3b64d068504c0e1ea20674c347d95df
Luc Grosheintz <luc.grosheintz@gmail.com>
2023-11-13 17:09:38 +0100

    fixup: collective hdf5

--- src/hdf5_collective_reader.cpp
+++ src/hdf5_collective_reader.cpp
@@ -1,3 +1,5 @@
+#include <bbp/sonata/hdf5_collective_reader.h>
+
 #include "population.hpp"
 #include <cstdint>
 #include <stdexcept>
@@ -61,9 +63,14 @@ HighFive::HyperSlab make_hyperslab(const std::vector<Range>& ranges) {
     return slab;
 }
 
-template <class Range>
-HighFive::HyperSlab make_hyperslab(const std::vector<Range>& xranges,
-                                   const std::array<size_t, 2>& yslice) {
+template <class XRange, class YRange>
+HighFive::HyperSlab make_hyperslab(const std::vector<XRange>& xranges,
+                                   const std::vector<YRange>& yranges) {
+    if(yranges.size() != 1) {
+        throw SonataError("Case `yranges.size() != 1` has not been implemented.");
+    }
+    const auto& yslice = yranges[0];
+
     HighFive::HyperSlab slab;
 
     size_t j_begin = std::get<0>(yslice);
@@ -77,79 +84,70 @@ HighFive::HyperSlab make_hyperslab(const std::vector<Range>& xranges,
     return slab;
 }
 
-template <class T, class Range>
-std::vector<T> readCanonicalSelection(const HighFive::DataSet& dset,
-                                      const std::vector<Range>& ranges,
-                                      const HighFive::DataTransferProps& dxpl) {
-    return dset.select(make_hyperslab(ranges)).template read<std::vector<T>>(dxpl);
-}
-
-template <class Range>
-std::vector<std::array<uint64_t, 2>> readCanonicalRow(const HighFive::DataSet& dset,
-                                                      const std::vector<Range>& ranges,
-                                                      const HighFive::DataTransferProps& dxpl) {
-    auto dataspace = HighFive::DataSpace({bulk_read::detail::flatSize(ranges), 2});
-    return dset.select(make_hyperslab(ranges, {0, 2}), dataspace)
-        .template read<std::vector<std::array<uint64_t, 2>>>(dxpl);
-}
-
-template <class T, class Range>
-std::vector<T> readSelection(const HighFive::DataSet& dset,
-                             const std::vector<Range>& ranges,
-                             const HighFive::DataTransferProps& dxpl) {
-    if (bulk_read::detail::isCanonical(ranges)) {
-        return readCanonicalSelection<T>(dset, ranges, dxpl);
-    }
-
-    // The fully general case:
-    //
-    // 1. Create a canonical selection and read into `linear_result`.
-    // 2. Copy values from the canonical `linear_results` to their final
-    //    destination.
-    auto canonicalRanges = bulk_read::sortAndMerge(ranges, 0);
-    auto linear_result = readCanonicalSelection<T>(dset, canonicalRanges, dxpl);
-
-    const auto ids = bulk_read::detail::flattenRanges(ranges);
-
-    std::vector<std::size_t> ids_index(ids.size());
-    std::iota(ids_index.begin(), ids_index.end(), std::size_t(0));
-    std::stable_sort(ids_index.begin(), ids_index.end(), [&ids](size_t i0, size_t i1) {
-        return ids[i0] < ids[i1];
-    });
-
-    std::vector<T> result(ids.size());
-    size_t linear_index = 0;
-    result[ids_index[0]] = linear_result[0];
-    for (size_t i = 1; i < ids.size(); ++i) {
-        if (ids[ids_index[i - 1]] != ids[ids_index[i]]) {
-            linear_index += 1;
-        }
-
-        result[ids_index[i]] = linear_result[linear_index];
-    }
-
-    return result;
-}
 }  // namespace detail
 
 template <class T>
-class Hdf5PluginReadCollective: virtual public Hdf5PluginCollectiveData,
-                                virtual public Hdf5PluginReadInterface<T>
+class Hdf5PluginRead1DCollective: virtual public Hdf5PluginCollectiveData,
+                                virtual public Hdf5PluginRead1DInterface<T>
 {
   public:
     std::vector<T> readSelection(const HighFive::DataSet& dset,
                                  const Selection& selection) const override {
-        return detail::readSelection<T>(dset, selection.ranges(), this->dxpl());
+        return dset.select(detail::make_hyperslab(selection.ranges())).template read<std::vector<T>>(this->dxpl());
     }
 };
 
 template <class T>
+class Hdf5PluginRead2DCollective: virtual public Hdf5PluginCollectiveData,
+                                virtual public Hdf5PluginRead2DInterface<T>
+{
+  private:
+    using AltRanges = std::vector<std::array<uint64_t, 2>>;
+
+  public:
+    std::vector<T> readSelection(const HighFive::DataSet& dset,
+                                 const Selection& xsel,
+                                 const Selection& ysel) const override {
+        return readSelectionImpl(dset, xsel.ranges(), ysel.ranges());
+    }
+
+    std::vector<T> readSelection(const HighFive::DataSet& dset,
+                                 const AltRanges& xsel,
+                                 const Selection& ysel) const override {
+        return readSelectionImpl(dset, xsel, ysel.ranges());
+    }
+
+    std::vector<T> readSelection(const HighFive::DataSet& dset,
+                                 const Selection& xsel,
+                                 const AltRanges& ysel) const override {
+        return readSelectionImpl(dset, xsel.ranges(), ysel);
+    }
+
+    std::vector<T> readSelection(const HighFive::DataSet& dset,
+                                 const AltRanges& xsel,
+                                 const AltRanges& ysel) const override {
+        return readSelectionImpl(dset, xsel, ysel);
+    }
+
+  private:
+    template <class XRange, class YRange>
+    std::vector<T> readSelectionImpl(const HighFive::DataSet& dset,
+                                     const std::vector<XRange>& xsel,
+                                     const std::vector<YRange>& ysel) const {
+        auto dataspace = HighFive::DataSpace({bulk_read::detail::flatSize(xsel), 2});
+        return dset.select(detail::make_hyperslab(xsel, ysel), dataspace)
+            .template read<std::vector<T>>(this->dxpl());
+    }
+};
+
+template <class T, class U>
 class Hdf5PluginCollective;
 
-template <class... Types>
-class Hdf5PluginCollective<std::tuple<Types...>>
-    : virtual public Hdf5PluginInterface<std::tuple<Types...>>,
-      virtual public Hdf5PluginReadCollective<Types>...
+template <class... Ts, class... Us>
+class Hdf5PluginCollective<std::tuple<Ts...>, std::tuple<Us...>>
+    : virtual public Hdf5PluginInterface<std::tuple<Ts...>, std::tuple<Us...>>,
+      virtual public Hdf5PluginRead1DCollective<Ts>...,
+      virtual public Hdf5PluginRead2DCollective<Us>...
 {
   public:
     Hdf5PluginCollective(MPI_Comm comm, bool collective_metadata, bool collective_transfer)
@@ -160,23 +158,12 @@ class Hdf5PluginCollective<std::tuple<Types...>>
                               HighFive::File::ReadOnly,
                               static_cast<const Hdf5PluginCollectiveData&>(*this).fapl());
     }
-
-    std::vector<std::array<uint64_t, 2>> readRows(const HighFive::DataSet& dset,
-                                                  const Selection& selection) const override {
-        return detail::readCanonicalRow(dset, selection.ranges(), this->dxpl());
-    }
-
-    std::vector<std::array<uint64_t, 2>> readRows(
-        const HighFive::DataSet& dset,
-        const std::vector<std::array<uint64_t, 2>>& ranges) const override {
-        return detail::readCanonicalRow(dset, ranges, this->dxpl());
-    }
 };
 
 Hdf5Reader make_collective_reader(MPI_Comm comm,
                                   bool collective_metadata,
                                   bool collective_transfer) {
-    return Hdf5Reader(std::make_shared<Hdf5PluginCollective<Hdf5Reader::supported_types>>(
+    return Hdf5Reader(std::make_shared<Hdf5PluginCollective<Hdf5Reader::supported_1D_types, Hdf5Reader::supported_2D_types>>(
         comm, collective_metadata, collective_transfer));
 }
 
