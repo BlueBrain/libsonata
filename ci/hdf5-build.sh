f1e083e601e9934dedd0cd0348c270b24fe84e0d
Mike Gevaert <michael.gevaert@epfl.ch>
2024-04-10 15:58:14 +0200

    try and build static macOS libraries

--- ci/hdf5-build.sh
+++ ci/hdf5-build.sh
@@ -5,34 +5,16 @@ set -e -x
 export INPUT=$(cd $(dirname "$1") && pwd -P)/$(basename "$1")
 export OUTPUT="$INPUT/install-$CIBW_ARCHS_MACOS"
 
-: "${ZLIB_VERSION:=1.3}"
-: "${UNIXY_AEC_VERSION:=1.0.4}"
-: "${UNIXY_HDF5_VERSION:=1.14.2}"
-
-function download_unpack_zlib {
-    pushd "$INPUT"
-    rm -rf "zlib-$ZLIB_VERSION"
-    echo "Downloading & unpacking ZLIB ${ZLIB_VERSION}"
-    curl -sLO https://zlib.net/fossils/zlib-$ZLIB_VERSION.tar.gz
-    tar xzf zlib-$ZLIB_VERSION.tar.gz
-    popd
-}
-
-function download_unpack_libaec {
-    pushd "$INPUT"
-    rm -rf "libaec-$UNIXY_AEC_VERSION"
-    echo "Downloading & unpacking aec ${UNIXY_AEC_VERSION}"
-    curl -fsSLO "https://gitlab.dkrz.de/k202009/libaec/uploads/ea0b7d197a950b0c110da8dfdecbb71f/libaec-${UNIXY_AEC_VERSION}.tar.gz"
-    tar zxf "libaec-$UNIXY_AEC_VERSION.tar.gz"
-    popd
-}
+: "${UNIXY_HDF5_VERSION:=1.14.3}"
 
 function download_unpack_hdf5 {
     pushd "$INPUT"
-    rm -rf hdf5-$UNIXY_HDF5_VERSION
-    echo "Downloading & unpacking HDF5 ${UNIXY_HDF5_VERSION}"
-    curl -fsSLO "https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-${UNIXY_HDF5_VERSION%.*}/hdf5-$UNIXY_HDF5_VERSION/src/hdf5-$UNIXY_HDF5_VERSION.tar.gz"
-    tar xzf "hdf5-$UNIXY_HDF5_VERSION.tar.gz"
+    local name=CMake-hdf5-$UNIXY_HDF5_VERSION.tar.gz
+    if [[ ! -e $name ]]; then
+        echo "Downloading & unpacking HDF5 ${UNIXY_HDF5_VERSION}"
+        curl -fsSLO "https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-${UNIXY_HDF5_VERSION%.*}/hdf5-$UNIXY_HDF5_VERSION/src/$name"
+    fi
+    tar xzf "$name"
     popd
 }
 
@@ -53,60 +35,27 @@ else
         export CFLAGS="$CFLAGS -arch $CIBW_ARCHS_MACOS"
         export CPPFLAGS="$CPPFLAGS -arch $CIBW_ARCHS_MACOS"
         export CXXFLAGS="$CXXFLAGS -arch $CIBW_ARCHS_MACOS"
-
-        download_unpack_zlib
-
-        pushd "$INPUT/zlib-$ZLIB_VERSION"
-        ./configure                             \
-            --archs="-arch $CIBW_ARCHS_MACOS"   \
-            --prefix="$OUTPUT"
-        make
-        make install
-        popd
-
-        ZLIB_ARG="--with-zlib=$OUTPUT"
-
-        if [[ "$CIBW_ARCHS_MACOS" = "arm64" ]]; then
-            HOST_ARG="--host=aarch64-apple-darwin"
-        fi
     fi
-
-    echo "Building & installing libaec"
-    download_unpack_libaec
-    pushd "$INPUT/libaec-$UNIXY_AEC_VERSION"
-    ./configure            \
-        --prefix="$OUTPUT" \
-        ${HOST_ARG}        \
-        --enable-static=no
-
-    make -j "$NPROC"
-    make install
-    popd
 
     echo "Building & installing hdf5"
     download_unpack_hdf5
-    pushd "$INPUT/hdf5-$UNIXY_HDF5_VERSION"
-    ./configure                        \
-        --prefix="$OUTPUT"             \
-        --with-szlib="$OUTPUT"         \
-        ${HOST_ARG}                    \
-        ${ZLIB_ARG}                    \
-        --enable-build-mode=production \
-        --enable-tools=no              \
-        --enable-tests=no              \
-        --enable-static=no
 
-    # {
-    # work around for crosscompile getting: `./H5detect: Bad CPU type in executable`
-    # so build it on the host architecture instead
-    pushd src
-    ( export ARCHFLAGS= ; make H5detect H5make_libsettings CFLAGS= CCLDFLAGS= CXXFLAGS= CPPFLAGS= )
-    popd
-    # }
-
+    install="$OUTPUT/install"
+    mkdir -p "$OUTPUT/build"
+    pushd "$OUTPUT/build"
+    cmake -G'Unix Makefiles' \
+        -DCMAKE_BUILD_TYPE:STRING=Release \
+        -DBUILD_SHARED_LIBS:BOOL=OFF \
+        -DHDF5_BUILD_UTILS:BOOL=OFF \
+        -DHDF5_BUILD_HL_LIB:BOOL=OFF \
+        -DHDF5_BUILD_EXAMPLES:BOOL=OFF \
+        -DBUILD_TESTING:BOOL=OFF \
+        -DHDF5_BUILD_TOOLS:BOOL=OFF \
+        -DCMAKE_INSTALL_PREFIX="$install" \
+        "$INPUT/CMake-hdf5-$UNIXY_HDF5_VERSION/hdf5-$UNIXY_HDF5_VERSION"
     make -j "$NPROC"
     make install
     popd
 
-    file "$OUTPUT"/lib/*
+    find "$OUTPUT"
 fi
